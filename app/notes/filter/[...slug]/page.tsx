df6188cf5a9f8333e8558bc137e6952c845491c9
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-06-28 21:10:47 +0300

    del opcional slug

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -2,7 +2,7 @@ import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
 
 interface FilteredNotesPageProps {
-  params: { slug?: string[] };
+  params: { slug: string[] };
 }
 
 export default async function FilteredNotesPage({
