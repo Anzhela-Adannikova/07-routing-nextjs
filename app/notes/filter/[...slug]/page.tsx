7abd88b5ff7ee319b6b8b38bcceb57110dadf886
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-06-28 22:07:37 +0300

    fixed type in page

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,14 +1,11 @@
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
 
-interface FilteredNotesPageProps {
-  params: { slug: string[] };
-}
+type Params = { params: { slug: string[] } };
 
-export default async function FilteredNotesPage({
-  params,
-}: FilteredNotesPageProps) {
-  const tag = params.slug?.[0];
+export default async function FilteredNotesPage({ params }: Params) {
+  const tag = params.slug[0];
+
   const isAll = !tag || tag === "All";
 
   const data = await fetchNotes(1, "", 12, isAll ? undefined : tag);
